711f29f5ac577467f21784e118cf7de40c44f485
krishnapotla” <krishnachaitanya.potla@gmail.com>
2019-05-06 06:43:03 +0530

    Code review and rework

--- quora-service/src/main/java/com/upgrad/quora/service/business/AdminBusinessService.java
+++ quora-service/src/main/java/com/upgrad/quora/service/business/AdminBusinessService.java
@@ -10,24 +10,26 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
+/**
+ * This class contains Admin related business operations
+ */
 @Service
 public class AdminBusinessService {
 
     @Autowired
     private UserDao userDao;
 
-
     @Transactional(propagation = Propagation.REQUIRED)
     public UserEntity deleteUser(final String userUuid, final String authorizationToken) throws AuthorizationFailedException,
             UserNotFoundException {
         UserAuthTokenEntity userAuthTokenEntity = userDao.getUserAuthToken(authorizationToken);
-        if(userAuthTokenEntity == null) {
+        if (userAuthTokenEntity == null) {
             throw new AuthorizationFailedException("ATHR-001", "User has not signed in");
         }
-        if(userAuthTokenEntity.getLogoutAt() != null) {
+        if (userAuthTokenEntity.getLogoutAt() != null) {
             throw new AuthorizationFailedException("ATHR-002", "User is signed out");
         }
-        if("nonadmin".equals(userAuthTokenEntity.getUser().getRole())) {
+        if ("nonadmin".equals(userAuthTokenEntity.getUser().getRole())) {
             throw new AuthorizationFailedException("ATHR-003", "Unauthorized Access, Entered user is not an admin");
         }
         UserEntity userEntity = userDao.getUser(userUuid);
@@ -37,6 +39,5 @@ public class AdminBusinessService {
         userDao.deleteUser(userEntity);
         return userEntity;
     }
-
 
 }
